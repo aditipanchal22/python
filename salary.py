697bdb292cfef91a8bc2d6a35d3d8bda037f1d73
aditipanchal22 <aditipanchal32@gmail.com>
2024-11-11 15:25:23 +0530

    Add files via upload

--- salary.py
+++ salary.py
@@ -1,1 +1,9 @@
-int(input(""))
+a=int(input("Enter sallary:"))
+n=int(input("enter your job experience in year:"))
+if a>30000 :
+       if(b>=2):
+              print("you are qualify for the loan")
+       else:
+              print("you must have been on your current job for at least two years")
+else:
+             print("you must earn atleast 30000 per year to qualify")
