d9533687c7a44081394fdb79215c69b8b5cd5030
aditipanchal22 <aditipanchal32@gmail.com>
2024-11-18 16:58:38 +0530

    Add files via upload

---
+++ sales.py
@@ -0,0 +1,9 @@
+a="yes"
+while a=="yes":
+   sales=float (int(input("enter the total sale:")))
+   comm_rate= rate=float(int(input("enter the commision rate:")))
+   commision=(sales*comm_rate)/100
+   print("commision",commision)
+   a=input("enter yes to continue:")
+
+
